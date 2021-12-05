984eb781e487b75eba4b7b8604d54e753973b4e9
nobel7761 <habiburnobel@gmail.com>
2021-12-06 03:21:04 +0600

    Connecting MongoDB

--- my-express/app.js
+++ my-express/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const studentRouter = require('./routers/studentRouter');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/my-students-2', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connection Successful"))
+    .catch(error => console.log("MongoDB Connection Failed"))
 
 app.use(express.json());
 app.use(morgan('dev'));
